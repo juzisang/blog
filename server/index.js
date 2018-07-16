a0b92051f2ffed090c3287a4ba64f728393cd0d0
juzisang <juziexe@gmail.com>
2018-07-16 11:19:50 +0800

    重构

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 
 const config = require('./config')
-const Db = require('./model')
 
-var app = express()
+const app = express()
 
 app.listen(config.app.port, function () {
   console.log('Example app listening on port 3000!')
