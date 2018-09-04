0ec51a047967dd14344f544b08efe74ac6870f3c
juzisang <juziexe@gmail.com>
2018-09-04 18:58:48 +0800

    clear all

--- client/components/layout/index.js
+++
@@ -1,6 +0,0 @@
-import BlogHeader from './Header.vue'
-import BlogFooter from './Footer.vue'
-
-module.exports = {
-  BlogHeader, BlogFooter
-}
--- client/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import config from '../config'
-import Vue from 'vue'
-
-axios.defaults.baseURL = config.baseURL
-
-Vue.prototype.$http = axios
-
-module.exports = axios
