25162fcba8bb927ed9fcec33e49ac45e85d171a9
juzisang <juziexe@gmail.com>
2018-12-05 20:05:18 +0800

    fix(*): del server all

--- server/src/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  app: {
-    port: process.env.PORT || 3000,
-  },
-};
--- server/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-import config from './config';
-
-app.listen(config.app.port, () => {
-  console.log(`start http://localhost:${config.app.port}`);
-});
