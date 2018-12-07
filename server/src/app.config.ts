4250d8b071b59e55f8ed70249961bb5781fb1e1b
juzisang <juziexe@gmail.com>
2018-12-07 14:49:46 +0800

    refactor(server): 更改 module 路径

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -1,33 +1,38 @@
-export default {
-  db: {
-    type: 'mysql',
-    host: process.env.BLOG_MYSQL_HOST || 'localhost',
-    port: process.env.BLOG_MYSQL_PORT || 3306,
-    username: process.env.BLOG_MYSQL_USER || 'developer',
-    password: process.env.BLOG_MYSQL_PASSWORD || 'developer',
-    database: process.env.BLOG_MYSQL_DATABASE || 'blog',
-    entities: ['src/**/**.entity{.ts,.js}'],
-    synchronize: true,
+/**
+ * DB配置
+ */
+export const DB = {
+  type: 'mysql',
+  host: process.env.BLOG_MYSQL_HOST || 'localhost',
+  port: process.env.BLOG_MYSQL_PORT || 3306,
+  username: process.env.BLOG_MYSQL_USER || 'developer',
+  password: process.env.BLOG_MYSQL_PASSWORD || 'developer',
+  database: process.env.BLOG_MYSQL_DATABASE || 'blog',
+  entities: ['src/**/**.entity{.ts,.js}'],
+  synchronize: true,
+};
+
+/**
+ * 初始化数据
+ */
+export const DEFAULT_DATA = {
+  user: {
+    name: process.env.BLOG_USER_NAME || 'root',
+    avatar: process.env.BLOG_USER_AVATAR || '',
+    slogan: process.env.BLOG_USER_SLOGAN || '',
+    password: process.env.BLOG_USER_PASSWORD || '123456',
   },
-  defaultData: {
-    user: {
-      name: process.env.BLOG_USER_NAME || 'root',
-      avatar: process.env.BLOG_USER_AVATAR || '',
-      slogan: process.env.BLOG_USER_SLOGAN || '',
-      password: process.env.BLOG_USER_PASSWORD || '123456',
-    },
-    option: {
-      title: process.env.BLOG_OPTION_NAME || '橘子的blog',
-      sub_title: process.env.BLOG_OPTION_SUB_TITLE || '橘子的blog',
-      keywords: process.env.BLOG_OPTION_KEYWORDS || [
-        'blog',
-        'html5',
-        'javascript',
-        'typescript',
-      ],
-      description: process.env.BLOG_OPTION_DESCRIPTION || '记录一些技术文章',
-      site_url: process.env.BLOG_OPTION_SITE_URL || 'https://www.juzisang.top/',
-      site_email: process.env.BLOG_OPTION_SITE_EMAIL || 'juziexe@gmail.com',
-    },
+  option: {
+    title: process.env.BLOG_OPTION_NAME || '橘子的blog',
+    sub_title: process.env.BLOG_OPTION_SUB_TITLE || '橘子的blog',
+    keywords: process.env.BLOG_OPTION_KEYWORDS || [
+      'blog',
+      'html5',
+      'javascript',
+      'typescript',
+    ],
+    description: process.env.BLOG_OPTION_DESCRIPTION || '记录一些技术文章',
+    site_url: process.env.BLOG_OPTION_SITE_URL || 'https://www.juzisang.top/',
+    site_email: process.env.BLOG_OPTION_SITE_EMAIL || 'juziexe@gmail.com',
   },
 };
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,12 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ArticleModule } from './article/article.module';
-import { CategoryModule } from './category/category.module';
-import { CommentModule } from './comment/comment.module';
-import { OptionModule } from './option/option.module';
-import { TagModule } from './tag/tag.module';
-import { UserModule } from './user/user.module';
+import { ArticleModule } from './modules/article/article.module';
+import { CategoryModule } from './modules/category/category.module';
+import { CommentModule } from './modules/comment/comment.module';
+import { OptionModule } from './modules/option/option.module';
+import { TagModule } from './modules/tag/tag.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
