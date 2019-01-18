91ffc583f2ed5a0b9e80cb9be8431706c4e00022
juzisang <juziexe@gmail.com>
2019-01-18 21:50:50 +0800

    refactor(code): update

--- server/src/interceptors/response.interceptor.ts
+++ server/src/interceptors/response.interceptor.ts
@@ -1,7 +1,6 @@
 import { NestInterceptor, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ResponseData } from '../utils/response.data';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
@@ -10,5 +9,17 @@ export class ResponseInterceptor implements NestInterceptor {
     call$: Observable<any>,
   ): Observable<any> {
     return call$.pipe(map(data => new ResponseData(data)));
+  }
+}
+
+export class ResponseData {
+  body: any;
+  statusCode: number;
+  message: string;
+
+  constructor(body: any, statusCode?: number, message?: string) {
+    this.statusCode = statusCode || 200;
+    this.message = message || this.statusCode === 200 ? 'ok' : 'error';
+    this.body = body;
   }
 }
