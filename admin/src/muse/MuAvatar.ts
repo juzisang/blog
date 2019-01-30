82fd3fd5448296c80ab4f6505d9ab0d8758136e4
juzisang <juziexe@gmail.com>
2019-01-31 00:16:31 +0800

    refactor(muse): code style

--- admin/src/muse/MuAvatar.ts
+++ admin/src/muse/MuAvatar.ts
@@ -1,8 +1,6 @@
 import * as Tsx from "vue-tsx-support";
 
-export const MuAvatar = Tsx.ofType<Props>().convert(
-  require("muse-ui/lib/Avatar").default
-);
+export const MuAvatar = Tsx.ofType<Props>().convert(require("muse-ui/lib/Avatar").default);
 
 type Props = {
   color?: string;
