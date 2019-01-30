3c628b06876ab2cbc01de8800ddbb54663b0fc86
juzisang <juziexe@gmail.com>
2019-01-31 00:43:55 +0800

    feat(muse): add List

--- admin/src/muse/MuList.ts
+++ admin/src/muse/MuList.ts
@@ -1,15 +1,41 @@
 import * as Tsx from 'vue-tsx-support';
 
-const _List = require('muse-ui/lib/List');
-
-export const MuList = Tsx.ofType<ListProps>().convert(_List.List);
-export const MuListItem = Tsx.ofType<ListProps>().convert(_List.ListItem);
-export const MuListAction = Tsx.ofType<ListProps>().convert(_List.ListAction);
+export const MuList = Tsx.ofType<ListProps, ListEvents>().convert(require('muse-ui/lib/List').List);
+export const MuListItem = Tsx.ofType<ListItemProps>().convert(require('muse-ui/lib/List').ListItem);
+export const MuListAction = Tsx.ofType().convert(require('muse-ui/lib/List').ListAction);
+export const MuListItemContent = Tsx.ofType().convert(require('muse-ui/lib/List').ListItemContent);
+export const MuListItemTitle = Tsx.ofType().convert(require('muse-ui/lib/List').ListItemTitle);
+export const MuListItemSubTitle = Tsx.ofType().convert(require('muse-ui/lib/List').ListItemSubTitle);
+export const MuListItemAfterText = Tsx.ofType().convert(require('muse-ui/lib/List').ListItemAfterText);
 
 type ListProps = {
-  value?: string;
-  size?: string | number;
-  left?: boolean;
-  right?: boolean;
-  color?: string;
+  textline?: 'two-line' | 'three-line';
+  dense?: boolean;
+  nestedIndent?: boolean;
+  toggleNested?: boolean;
+  toggleNestedType?: 'expand' | 'popover';
+  value?: any;
+};
+
+type ListEvents = {
+  change: any;
+};
+
+type ListItemProps = {
+  button?: boolean;
+  avatar?: boolean;
+  nested?: boolean;
+  nestedListClass?: boolean;
+  open?: boolean;
+  value?: any;
+  ripple?: boolean;
+  href?: string;
+  to?: string | object;
+  tag?: string;
+  activeClass?: string;
+  event?: string | Array<string>;
+  exact?: boolean;
+  exactActiveClass?: string;
+  append?: boolean;
+  replace?: boolean;
 };
