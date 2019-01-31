917fe6b15e14aaa59c159aa6e3776f9e89fc3517
juzisang <juziexe@gmail.com>
2019-01-31 15:07:20 +0800

    chore(config): update

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -3,15 +3,43 @@ const path = require('path');
 module.exports = {
   productionSourceMap: false,
   chainWebpack(config) {
-    setAlias(config);
+    // config.resolve.alias.set("src", path.resolve(__dirname, "src"));
+    // 热重载
     if (process.env.NODE_ENV !== 'production') {
-      hotReload(config);
+      config.module
+        .rule('tsx')
+        .test(/\.tsx$/)
+        .use('vue-jsx-hot-loader')
+        .before('babel-loader')
+        .loader('vue-jsx-hot-loader');
     }
+    // bundle-analyzer
     if (process.env.npm_config_report) {
-      openBundleAnalyzer(config);
+      config.plugin('webpack-bundle-analyzer').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
     }
     ignoreCssWarnings(config);
-    cssModules(config);
+    // 生成 css.d.ts
+    ['css', 'less', 'scss', 'sass', 'stylus', 'postcss'].forEach(rule => {
+      ['normal-modules'].forEach(oneOf => {
+        config.module
+          .rule(rule)
+          .oneOf(oneOf)
+          .uses.delete('css-loader');
+        config.module
+          .rule(rule)
+          .oneOf(oneOf)
+          .use('typings-for-css-modules-loader')
+          .before('postcss-loader')
+          .loader('typings-for-css-modules-loader')
+          .options({
+            modules: true,
+            namedExport: true,
+            camelCase: true,
+            importLoaders: 2,
+            localIdentName: process.env.NODE_ENV !== 'production' ? '[local]-[hash:base64:5]' : '[hash:base64:5]',
+          });
+      });
+    });
   },
   devServer: {
     proxy: {
@@ -36,14 +64,12 @@ function ignoreCssWarnings(config) {
     constructor(options) {
       this.options = options;
     }
-
     apply(compiler) {
       compiler.hooks.afterEmit.tap('FilterPlugin', compilation => {
         compilation.warnings = compilation.warnings.filter(warning => !this.options.filter.test(warning.message));
       });
     }
   }
-
   config
     .plugin('filter-css-warnings-plugin')
     .use(
@@ -52,57 +78,4 @@ function ignoreCssWarnings(config) {
       }),
     )
     .end();
-}
-
-/**
- * 设置别名
- */
-function setAlias(config) {
-  // config.resolve.alias.set("src", path.resolve(__dirname, "src"));
-}
-
-/**
- * 启动webpack-bundle-analyzer
- */
-function openBundleAnalyzer(config) {
-  config.plugin('webpack-bundle-analyzer').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
-}
-
-/**
- * 热重载
- */
-function hotReload(config) {
-  config.module
-    .rule('tsx')
-    .test(/\.tsx$/)
-    .use('vue-jsx-hot-loader')
-    .before('babel-loader')
-    .loader('vue-jsx-hot-loader');
-}
-
-/**
- * 自动生成 css.d.ts
- */
-function cssModules(config) {
-  ['css', 'less', 'scss', 'sass', 'stylus', 'postcss'].forEach(rule => {
-    ['normal-modules'].forEach(oneOf => {
-      config.module
-        .rule(rule)
-        .oneOf(oneOf)
-        .uses.delete('css-loader');
-      config.module
-        .rule(rule)
-        .oneOf(oneOf)
-        .use('typings-for-css-modules-loader')
-        .before('postcss-loader')
-        .loader('typings-for-css-modules-loader')
-        .options({
-          modules: true,
-          namedExport: true,
-          camelCase: true,
-          importLoaders: 2,
-          localIdentName: process.env.NODE_ENV !== 'production' ? '[local]-[hash:base64:5]' : '[hash:base64:5]',
-        });
-    });
-  });
 }
