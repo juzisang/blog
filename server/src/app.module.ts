74807285854d8b7ec648b4ade9b35b9cd10e8c99
juzisang <juziexe@gmail.com>
2020-05-21 19:35:02 +0800

    feat: add env config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local', '.env.production', '.env.development']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entityPrefix: 'blog_',
+      charset: 'utf8_general_ci',
+      entities: [path.join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      logging: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
