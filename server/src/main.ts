152f6fbeab92e1c600f6f215a322e44609fa7562
juzisang <juziexe@gmail.com>
2020-05-22 18:04:48 +0800

    feat: add validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: process.env.NODE_ENV === 'development' ? '*' : process.env.BLOG_SITE_ORIGIN,
     credentials: true,
     maxAge: 60 * 60 * 24,
   });
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
