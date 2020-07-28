a4b7290dbacfcf605869c147061cf8678ab9f67e
juzisang <juziexe@gmail.com>
2020-07-28 10:29:36 +0800

    ci: 优化打包流程

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,15 @@
+const path = require('path')
+const fs = require('fs')
+const dotenv = require('dotenv')
+
 module.exports = {
   apps: [{
-    name: 'blog-server',
+    name: path.basename(__dirname),
+    cwd: __dirname,
     script: './dist/main.js',
     max_memory_restart: '300M',
     autorestart: false,
-    watch: false
+    watch: false,
+    env: dotenv.parse(fs.readFileSync(path.join(__dirname, '.env')))
   }]
 };
