10a873af61f1c717f7cc25d70fcc189fb26837d8
juzisang <juziexe@gmail.com>
2021-05-01 11:28:11 +0000

    perf: 设置路由前缀

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,17 +8,19 @@ import { TransformInterceptor } from './util/transform.interceptor'
 console.log('config:', config)
 
 async function bootstrap () {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule,{})
+  // 设置路由前缀
+  app.setGlobalPrefix('api')
   // 过滤常见安全问题
   app.use(helmet())
   // 设置参数验证
   app.useGlobalPipes(new ValidationPipe())
   // cors
-  app.enableCors({
-    origin: config.NODE_ENV === 'development' ? '*' : config.BLOG_SITE_ORIGIN,
-    credentials: true,
-    maxAge: 60 * 60 * 24,
-  })
+  // app.enableCors({
+  //   origin: config.NODE_ENV === 'development' ? '*' : config.BLOG_SITE_ORIGIN,
+  //   credentials: true,
+  //   maxAge: 60 * 60 * 24,
+  // })
   // 将数据格式转换位固定格式
   app.useGlobalInterceptors(new TransformInterceptor())
 
